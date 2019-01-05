77a6f440f657027c9b861c11a86243ae3edd04de
Changnam Hong <hcn1519@gmail.com>
2019-01-05 20:19:58 +0900

    Run React Native Base Sample

--- ios/ReactNativeStarter/ViewController.swift
+++ ios/ReactNativeStarter/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import React
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func highScoreButtonTapped(sender : UIButton) {
 
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")
+        let mockData:NSDictionary = ["scores":
+            [
+                ["name":"Alex", "value":"42"],
+                ["name":"Joel", "value":"10"]
+            ]
+        ]
+
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "RNHighScores",
+            initialProperties: mockData as [NSObject : AnyObject],
+            launchOptions: nil
+        )
+        let vc = UIViewController()
+        vc.view = rootView
+        self.present(vc, animated: true, completion: nil)
+    }
 }
 
