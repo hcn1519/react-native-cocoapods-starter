0eb0aaea936e34abb6a5114a50a051eff7ad27b1
Changnam Hong <hcn1519@gmail.com>
2019-01-05 20:12:45 +0900

    Install ReactNative Using Cocoapods in iOS Project

---
+++ ios/ReactNativeStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReactNativeStarter
+//
+//  Created by 홍창남 on 05/01/2019.
+//  Copyright © 2019 홍창남. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
